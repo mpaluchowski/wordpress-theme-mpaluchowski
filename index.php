b1b21674ca76f072de41bcc187758c703fca0da3
Michal Paluchowski <michal@paluchowski.com>
2014-03-17 22:34:20 +0100

    Putting a homepage link on the header
    The header/page name is the only thing showing on phones in the header and
    there it's the only way to get back to the homepage. Plus, it's
    potentially good SEO.

--- index.php
+++ index.php
@@ -27,10 +27,18 @@
 		<?php endif; ?>
 
 		<?php if ( is_single() ): ?>
-			<div id="site-title" itemprop="headline"><?php bloginfo( 'name' ); ?></div>
+			<div id="site-title" itemprop="headline">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div>
 			<div id="site-description" itemprop="description"><?php bloginfo( 'description' ); ?></div>
 		<?php else: ?>
-			<h1 itemprop="headline"><?php bloginfo( 'name' ); ?></h1>
+			<h1 itemprop="headline">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</h1>
 			<h2 itemprop="description"><?php bloginfo( 'description' ); ?></h2>
 		<?php endif; ?>
 

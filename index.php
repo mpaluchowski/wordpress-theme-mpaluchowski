06cf561411f48f1fe109a17a1b590b92473d99ec
Michal Paluchowski <michal@paluchowski.com>
2015-11-02 08:46:59 +0100

    Correcting itemprop value for main content

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 		</div>
 	</header>
 
-	<main role="main" itemprop="mainContentOfPage" itemscope itemtype="http://schema.org/Blog">
+	<main role="main" itemprop="mainEntity" itemscope itemtype="http://schema.org/Blog">
 		<?php
 			if ( have_posts() ) :
 				while ( have_posts() ) : the_post();
